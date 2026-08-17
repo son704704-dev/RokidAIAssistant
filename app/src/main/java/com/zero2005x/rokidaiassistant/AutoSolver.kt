ac1bffb929bfa5cde8e84f7dd82bb8c2d68b019c
son704704-dev <son704704@gmail.com>
2026-08-17 19:31:35 +0900

    Update AutoSolver.kt

--- app/src/main/java/com/zero2005x/rokidaiassistant/AutoSolver.kt
+++ app/src/main/java/com/zero2005x/rokidaiassistant/AutoSolver.kt
@@ -1,11 +1,8 @@
 package com.zero2005x.rokidaiassistant
 
 import kotlinx.coroutines.*
-import android.os.Handler
-import android.os.Looper
 
-// 기존 프로젝트의 기능을 그대로 가져다 쓰는 자동화 클래스
-class AutoSolver(private val mainActivity: MainActivity) {
+class AutoSolver(private val onTrigger: () -> Unit) {
     private var isRunning = false
     private val scope = CoroutineScope(Dispatchers.Main)
 
@@ -19,8 +16,7 @@ class AutoSolver(private val mainActivity: MainActivity) {
     private fun startLoop() {
         scope.launch {
             while (isRunning) {
-                // 기존 프로젝트의 촬영 함수 호출 (MainActivity 내의 함수 접근)
-                mainActivity.takePictureAndAnalyze() 
+                onTrigger()
                 delay(10000) // 10초 대기
             }
         }
